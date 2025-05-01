bbef2d7c5114472eab7e98fd0476c319d55807c0
Thembisile  Makamu <makamuthe2@gmail.com>
2025-05-01 13:26:12 +0200

    Refactor project structure: remove landing page, add dashboard and index pages, update Firebase configuration to use environment variables

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
   const firebaseConfig = {
-    apiKey: "AIzaSyAGaZKB4VAmQRV-wsJanZhEmTz5QNLBfD0",
-    authDomain: "ai-fluency-1cd86.firebaseapp.com",
-    projectId: "ai-fluency-1cd86",
-    storageBucket: "ai-fluency-1cd86.firebasestorage.app",
-    messagingSenderId: "230668708493",
-    appId: "1:230668708493:web:48ade94203de75fa94d0c4"
+    apiKey: "process.env.API_KEY",
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: "process.env.STORAGE_BUCKET",
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
   };
 
   // Initialize Firebase
